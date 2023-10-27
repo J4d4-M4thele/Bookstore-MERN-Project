6d7406cf923614a02121e971a6b37b14998424dd
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-27 09:57:30 +0200

    update

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,2 @@
 export const PORT = 5555;
+export const mongoDBURL = 'mongodb+srv://JadaMathele:kPfIAdYSqOfix3ap@testcluster1.wxdgvkq.mongodb.net/';
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 import express from 'express';
-import { PORT } from './config.js';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+
+mongoose.connect(mongoDBURL)
+.then (() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+        console.log(`App is running on Port: ${PORT}`)
+    });
+})
+.catch ((err) => {
+    console.log(err);
+});
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App is running on Port: ${PORT}`)
+app.get('/', (req,res) => {
+    console.log(req);
+    return res.status(234).send('Welcome to MERN Stack bookstore')
 });
+
