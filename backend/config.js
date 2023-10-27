92625b23d756d09af3a592250fbe076ee11dc7a7
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-27 20:01:18 +0200

    update

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,7 @@
 export const PORT = 5555;
-export const mongoDBURL = 'mongodb+srv://JadaMathele:kPfIAdYSqOfix3ap@testcluster1.wxdgvkq.mongodb.net/';
+
+export const mongoDBURL =
+'mongodb+srv://JadaMathele:kPfIAdYSqOfix3ap@testcluster1.wxdgvkq.mongodb.net/';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
--- backend/index.js
+++ backend/index.js
@@ -1,50 +1,41 @@
 import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
-import  { Book } from './model/bookModel.js'
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
+// Middleware for parsing request body
 app.use(express.json());
 
-mongoose.connect(mongoDBURL)
-.then (() => {
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
+});
+
+app.use('/books', booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
     console.log('App connected to database');
     app.listen(PORT, () => {
-        console.log(`App is running on Port: ${PORT}`);
+      console.log(`App is listening to port: ${PORT}`);
     });
-})
-.catch ((err) => {
-    console.log(err);
-});
-
-app.get('/', (req,res) => {
-    console.log(req);
-    return res.status(234).send('Welcome to MERN Stack bookstore')
-});
-
-//route for saving a new book
-app.post('/books', async (req, res) => {
-    try {
-        if(
-            !req.body.title ||
-            !req.body.author ||
-            !req.body.publishYear
-        ) {
-            return res.status(400).send({
-                message: 'Send all required fields: title, author and publishYear'
-            });
-        };
-        const book= new Book({
-            title: req.body.title,
-            author: req.body.author,
-            publishYear: req.body.publishYear
-        });
-        //creating book
-        await book.save();
-        return res.status(201).send(book);
-    } catch (err) {
-        console.log(err.message);
-        return res.status(500).send({message: err.message});
-    }
-});
+  })
+  .catch((error) => {
+    console.log(error);
+  });
