a427fdb054239566e2107e49ec01254006141259
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-27 10:26:11 +0200

    update

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
+import  {Book } from './model/bookModel.js'
 
 mongoose.connect(mongoDBURL)
 .then (() => {
@@ -20,3 +21,28 @@ app.get('/', (req,res) => {
     return res.status(234).send('Welcome to MERN Stack bookstore')
 });
 
+//route for saving a new book
+app.post('/books', async (req, res) => {
+    try {
+        if(
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear
+        ) {
+            return res.status(400).send({
+                message: 'Send all required fields: title, author and publishYear'
+            });
+        };
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear
+        };
+        //creating book
+        const book = await Book.create(newBook);
+        return res.status(201).send(book);
+    } catch (err) {
+        console.log(err.message);
+        return res.status(500).send({message: err.message});
+    }
+});
